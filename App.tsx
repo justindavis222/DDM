b6ce376193ce848848b5715a1574221f05945006
justindavis222 <justindavis73089@gmail.com>
2023-04-22 18:57:35 -0400

    adding the function for dynamic data model

--- App.tsx
+++ App.tsx
@@ -1,117 +1,170 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- */
-
-import React from 'react';
-import type {PropsWithChildren} from 'react';
+import React, {useState, useEffect} from 'react';
 import {
   SafeAreaView,
   ScrollView,
   StatusBar,
   StyleSheet,
   Text,
-  useColorScheme,
   View,
+  TextInput,
+  TouchableOpacity,
 } from 'react-native';
+import {Picker} from '@react-native-picker/picker';
+import dataModel1 from './src/models/dataModel1.json';
+import dataModel2 from './src/models/dataModel2.json';
+import CryptoJS from 'crypto-js';
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
-
-type SectionProps = PropsWithChildren<{
-  title: string;
-}>;
-
-function Section({children, title}: SectionProps): JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
-  return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
-  );
+interface Field {
+  label: string;
+  type: 'int' | 'float' | 'string';
+  readonly: boolean;
+  calculate: string | null;
+  value?: string;
 }
 
-function App(): JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
+type Fields = {
+  [key: string]: Field;
+};
 
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
+const App = () => {
+  const [selectedModel, setSelectedModel] = useState<any>(null);
+  const [fields, setFields] = useState<Fields>({});
+
+  useEffect(() => {
+    if (selectedModel) {
+      setFields(selectedModel.fields);
+    }
+  }, [selectedModel]);
+
+  const handleInputChange = (fieldName: string, value: string) => {
+    const newFields = {...fields};
+    newFields[fieldName].value = value;
+    setFields(newFields);
+  };
+
+  const customCalculate = (key: string, f: Fields) => {
+    if (selectedModel.name === 'Hash Combination' && key === 'hash') {
+      return calculateSha256Hash(f.string1.value || '', f.string2.value || '');
+    } else if (selectedModel.name === 'Statistics') {
+      const values = Object.values(f)
+        .filter(field => !field.readonly)
+        .map(field => parseFloat(field.value || '0'));
+
+      if (key === 'mean') {
+        const sum = values.reduce((a, b) => a + b, 0);
+        return (sum / values.length).toFixed(2);
+      } else if (key === 'median') {
+        const sorted = values.sort((a, b) => a - b);
+        const mid = Math.floor(sorted.length / 2);
+        return sorted.length % 2 !== 0
+          ? sorted[mid].toFixed(2)
+          : ((sorted[mid - 1] + sorted[mid]) / 2).toFixed(2);
+      } else if (key === 'std_deviation') {
+        const mean = values.reduce((a, b) => a + b, 0) / values.length;
+        const variance =
+          values.reduce((sum, value) => sum + Math.pow(value - mean, 2), 0) /
+          values.length;
+        return Math.sqrt(variance).toFixed(2);
+      }
+    }
+  };
+
+  const calculateValues = () => {
+    if (selectedModel) {
+      const updatedFields = {...fields};
+      for (const key in updatedFields) {
+        const field = updatedFields[key];
+        if (field.calculate) {
+          const calculatedValue = customCalculate(key, fields)!;
+          handleInputChange(key, calculatedValue);
+        }
+      }
+    }
+  };
+
+  const calculateSha256Hash = (input1: string, input2: string) => {
+    const combined = input1 + '\0' + input2;
+    return CryptoJS.SHA256(combined).toString();
   };
 
   return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar
-        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
-        backgroundColor={backgroundStyle.backgroundColor}
-      />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
+    <SafeAreaView style={styles.container}>
+      <StatusBar barStyle="dark-content" />
+      <ScrollView contentInsetAdjustmentBehavior="automatic">
+        <View>
+          <Text style={styles.title}>Dynamic Data Model App</Text>
+          <Picker
+            selectedValue={selectedModel}
+            onValueChange={(itemValue: any) => setSelectedModel(itemValue)}>
+            <Picker.Item label="Select a data model..." value={null} />
+            <Picker.Item label="Data Model 1" value={dataModel1} />
+            <Picker.Item label="Data Model 2" value={dataModel2} />
+          </Picker>
+          {selectedModel &&
+            Object.entries(fields).map(([key, field]: [string, Field]) => (
+              <View key={key} style={styles.fieldWrapper}>
+                <Text style={styles.fieldLabel}>{field.label}:</Text>
+                <TextInput
+                  style={styles.fieldInput}
+                  keyboardType={field.type === 'int' ? 'number-pad' : 'default'}
+                  onChangeText={(text: string) => handleInputChange(key, text)}
+                  value={field.value}
+                  editable={!field.readonly}
+                />
+              </View>
+            ))}
+          <TouchableOpacity
+            style={styles.calculateButton}
+            onPress={calculateValues}>
+            <Text style={styles.calculateButtonText}>Calculate</Text>
+          </TouchableOpacity>
         </View>
       </ScrollView>
     </SafeAreaView>
   );
-}
+};
 
 const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
+  container: {
+    flex: 1,
+    backgroundColor: '#F5FCFF',
   },
-  sectionTitle: {
+  title: {
     fontSize: 24,
-    fontWeight: '600',
+    fontWeight: 'bold',
+    textAlign: 'center',
+    marginTop: 20,
+    marginBottom: 20,
   },
-  sectionDescription: {
-    marginTop: 8,
+  fieldWrapper: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    paddingHorizontal: 20,
+    marginBottom: 10,
+  },
+  fieldLabel: {
     fontSize: 18,
-    fontWeight: '400',
   },
-  highlight: {
-    fontWeight: '700',
+  fieldInput: {
+    flex: 1,
+    marginLeft: 10,
+    fontSize: 18,
+    borderBottomWidth: 1,
+    borderColor: '#999',
+    padding: 5,
+  },
+  calculateButton: {
+    backgroundColor: '#3498db',
+    padding: 10,
+    margin: 20,
+    borderRadius: 5,
+  },
+  calculateButtonText: {
+    color: '#fff',
+    fontSize: 18,
+    fontWeight: 'bold',
+    textAlign: 'center',
   },
 });
 
